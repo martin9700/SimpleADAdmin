edf50480af9cc1895c18865b3446d0c3ab738b5c
Martin Pugh <martin@pughspace.com>
2016-08-10 12:13:03 -0400

    Progress
    1. Fixed object display
    2. Added Unlock, GetGroups and RemoveGroup methods
    3. Began work on AddGroup but this is early

--- Get-SAUser.ps1
+++ Get-SAUser.ps1
@@ -6,25 +6,25 @@
         This is a more advanced version of Get-ADUser.  It allows you to see administrator specific fields in the default view, but
         also saves the user object in a global variable:
 
-        $AHUser
+        $SAUser
 
         The advantage of this is if you want to do something to the object it's now persistent in your session.  Available methods are:
 
         AddGroup
-            Usage: $AHUser.AddGroup("NameOfGroup")
+            Usage: $SAUser.AddGroup("NameOfGroup")
         FindLockout
-            Usage: $AHUser.FindLockedout()
+            Usage: $SAUser.FindLockedout()
         GetGroups
-            Usage: $AHUser.GetGroups()
+            Usage: $SAUser.GetGroups()
         GetLastLogon
-            Usage: $AHUser.GetLastLogon()
+            Usage: $SAUser.GetLastLogon()
         RemoveGroup
-            Usage: $AHUser.RemoveGroup("NameOfGroup")
+            Usage: $SAUser.RemoveGroup("NameOfGroup")
         ResetPassword
-            Usage: $AHUser.ResetPassword()
+            Usage: $SAUser.ResetPassword()
                    Method will then prompt for new password
         Unlock
-            Usage: $AHUser.Unlock()
+            Usage: $SAUser.Unlock()
 
 
         *** Requires AH.Automation Module ***
@@ -35,10 +35,10 @@
     .INPUTS
         None
     .OUTPUTS
-        $AHUser PSCustomObject
+        $SAUser PSCustomObject
     .EXAMPLE
         Get-AHUser mpugh
-        $AHUser.Unlock()
+        $SAUser.Unlock()
 
         Get user information for mpugh, see that the account is locked.  Use the Unlock() method to unlock the account.
 
@@ -63,42 +63,25 @@
         [string]$User
     )
 
-    $PropertyList = @(
-        "Name"
-        "SamAccountName"
-        "Title"
-        "Description"
-        "GivenName"
-        "Surname"
-        "Manager"
-        "PasswordNeverExpires"
-        "PasswordNotRequired"
-        "distinguishedName"
-        "UserPrincipalName"
-        "ObjectClass"
-        "ObjectGUID"
-        "SID"
-    )
-
     $Searcher = [ADSISearcher]"(&(objectCategory=person)(objectClass=user)(samAccountName=$User))"
-    $AH = $Searcher.FindOne()
-    If ($AH.Count -eq 0)
+    $Found = $Searcher.FindOne()
+    If ($Found.Count -eq 0)
     {
-        Write-Verbose "No user matching $User, trying for a match"
+        Write-Verbose "No user matching $SAUser, trying for a match"
         $Searcher = [ADSISearcher]"(&(objectCategory=person)(objectClass=user)(displayName=*$User*))"
-        $AH = $Searcher.FindAll()
-        If ($AH.Count -gt 1)
+        $Found = $Searcher.FindAll()
+        If ($Found.Count -gt 1)
         {
             #Found more than one, need to select which one you want
-            $Selected = $AH | Select @{Name="SamAccountName";Expression={ $_.properties.samaccountname }},@{Name="DisplayName";Expression={ $_.properties.displayname }} | Out-GridView -Title "Select the user you want" -PassThru
+            $Selected = $Found | Select @{Name="SamAccountName";Expression={ $_.properties.samaccountname }},@{Name="DisplayName";Expression={ $_.properties.displayname }} | Out-GridView -Title "Select the user you want" -PassThru
             If (@($Selected).Count -eq 0)
             {
                 Write-Error "No user found, exiting" -ErrorAction Stop
             }
             $Searcher = [ADSISearcher]"(&(objectCategory=person)(objectClass=user)(samAccountName=$($Selected.SamAccountName)))"
-            $AH = $Searcher.FindOne()
+            $Found = $Searcher.FindOne()
         }
-        If ($AH.Count -eq 0)
+        If ($Found.Count -eq 0)
         {
             Write-Error "No user found, exiting" -ErrorAction Stop
         }
@@ -110,41 +93,30 @@
     $ACCOUNT_LOCKOUT           = 16
     $ADS_UF_PASSWD_NOTREQD     = 0x0020
 
-    $Global:AHUser = [PSCustomObject]@{
-        Name = $AH.properties.displayname[0]
-        SamAccountName = $AH.properties.samaccountname[0]
-        #Enabled = -not [bool]($AH.userAccountControl -band $ACCOUNTDISABLE)
-        #LockedOut = ($AH.properties.lockouttime[0] -gt 0)
-        Title = $AH.properties.title[0]
-        Description = $AH.properties.description[0]
-        GivenName = $AH.properties.givenname[0]
-        Surname = $AH.properties.sn[0]
-        PasswordNeverExpires = [bool]($AH.userAccountControl -band $DONT_EXPIRE_PASSWORD)
-        PasswordNotRequired = [bool]($AH.userAccountControl -band $ADS_UF_PASSWD_NOTREQD)
-        DistinguishedName = $AH.properties.distinguishedname[0]
-        UserPrincipalName = $AH.properties.userprincipalname[0]
-        ObjectGUID = New-Object GUID(,$AH.properties.objectguid[0])
-        ObjectSID = (New-Object System.Security.Principal.SecurityIdentifier($AH.properties.objectsid[0],0)).Value
-        #MemberOf = "" #$AH.properties.memberof
-        Manager = $AH.properties.manager[0]
+    $Global:SAUser = [PSCustomObject]@{
+        Name = $Found.properties.displayname[0]
+        SamAccountName = $Found.properties.samaccountname[0]
+        Title = $Found.properties.title[0]
+        Description = $Found.properties.description[0]
+        GivenName = $Found.properties.givenname[0]
+        Surname = $Found.properties.sn[0]
+        PasswordNeverExpires = [bool]($Found.userAccountControl -band $DONT_EXPIRE_PASSWORD)
+        PasswordNotRequired = [bool]($Found.userAccountControl -band $ADS_UF_PASSWD_NOTREQD)
+        DistinguishedName = $Found.properties.distinguishedname[0]
+        UserPrincipalName = $Found.properties.userprincipalname[0]
+        ObjectGUID = New-Object GUID(,$Found.properties.objectguid[0])
+        ObjectSID = (New-Object System.Security.Principal.SecurityIdentifier($Found.properties.objectsid[0],0)).Value
+        MemberOf = $Found.properties.memberof
+        Manager = $Found.properties.manager[0]
         LastLogon = ""
-        #LockedOut = ($AH.properties.lockouttime[0] -gt 0)
-        #BadPasswordCount = $AH.properties.badpwdcount[0]
-        #PasswordExpired = [bool]($AH.userAccountControl -band $PASSWORD_EXPIRED)
-        #PasswordLastSet = [DateTime]::FromFileTime($AH.properties.pwdlastset[0])
+        LockedOut = ($Found.properties.lockouttime[0] -gt 0)
+        BadPasswordCount = $Found.properties.badpwdcount[0]
+        PasswordExpired = [bool]($Found.userAccountControl -band $PASSWORD_EXPIRED)
+        PasswordLastSet = [DateTime]::FromFileTime($Found.properties.pwdlastset[0])
     }
 
-    $PropertySet = "LockedOut","BadPasswordCount","PasswordExpired","PasswordLastSet","MemberOf"
-    $Properties = $AHUser | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name
-    ForEach ($Property in $PropertySet)
-    {
-        If ($Properties -notcontains $Property)
-        {
-            $AHUser | Add-Member -Force -MemberType NoteProperty -Name $Property -Value ""
-        }
-    }
-
-    $AHUser | Add-Member -MemberType ScriptProperty -Force -Name Enabled -Value { 
+    #Add Enable property, and dynamically retrieve other information
+    $SAUser | Add-Member -MemberType ScriptProperty -Force -Name Enabled -Value { 
         $ACCOUNTDISABLE            = 0x000002
         $DONT_EXPIRE_PASSWORD      = 0x010000
         $PASSWORD_EXPIRED          = 0x800000
@@ -152,32 +124,103 @@
         $ADS_UF_PASSWD_NOTREQD     = 0x0020
 
         $Searcher = [ADSISearcher]"(&(objectCategory=person)(objectClass=user)(samAccountName=$($this.SamAccountName)))"
-        $AH = $Searcher.FindOne()
+        $Found = $Searcher.FindOne()
 
-        -not [bool]($AH.userAccountControl -band $ACCOUNTDISABLE)
+        -not [bool]($Found.userAccountControl -band $ACCOUNTDISABLE)
 
-        $this.LockedOut = ($AH.properties.lockouttime[0] -gt 0)
-        $this.BadPasswordCount = $AH.properties.badpwdcount[0]
-        $this.PasswordExpired = [bool]($AH.userAccountControl -band $PASSWORD_EXPIRED)
-        $this.PasswordLastSet = [DateTime]::FromFileTime($AH.properties.pwdlastset[0])
+        $this.LockedOut = ($Found.properties.lockouttime[0] -gt 0)
+        $this.BadPasswordCount = $Found.properties.badpwdcount[0]
+        $this.PasswordExpired = [bool]($Found.userAccountControl -band $PASSWORD_EXPIRED)
+        $this.PasswordLastSet = [DateTime]::FromFileTime($Found.properties.pwdlastset[0])
+        $this.MemberOf = $Found.properties.memberof
     }
-    $AHUser | Add-Member -Force -MemberType MemberSet PSStandardMembers ([System.Management.Automation.PSMemberInfo[]]@(New-Object System.Management.Automation.PSPropertySet("DefaultDisplayPropertySet",[String[]]@("Name","SamAccountName","Title","Enabled","LockedOut","BadPasswordCount","LastLogon"))))
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name Unlock -Value { 
-        Unlock-ADAccount -Identity $this.SamAccountName 
+
+    #Set Default View values
+    $SAUser | Add-Member -Force -MemberType MemberSet PSStandardMembers ([System.Management.Automation.PSMemberInfo[]]@(New-Object System.Management.Automation.PSPropertySet("DefaultDisplayPropertySet",[String[]]@("Name","SamAccountName","Title","Enabled","LockedOut","BadPasswordCount","LastLogon"))))
+    
+    #Add user Unlock method
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name Unlock -Value { 
+        $Found = [ADSI]"LDAP://$($this.DistinguishedName)"
+        $Found.Put("lockouttime",0)
+        $Found.SetInfo()
     }
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name GetGroups -Value { 
+
+    #GetGroups method, allows you to see just group name, includes a filter overload
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name GetGroups -Value { 
         Param (
             [string]$Filter = ".*"
         )
 
-        Write-Verbose "Group Memberships for: $($this.Name)" -Verbose
         $Groups = ForEach ($Group in $this.MemberOf)
         {
-            Get-ADGroup $Group | Select -ExpandProperty Name
+            If ($Group -match "cn=(?<GroupName>.*?)(?<!\\),")
+            {
+                $GroupName = $Matches.GroupName
+                If ($GroupName -match $Filter)
+                {
+                    $GroupName
+                }
+            }
         }
-        $Groups | Select-String $Filter | Sort
+        $Groups | Sort
     }
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name GetLastLogon -Value {
+
+    #Remove Group method, includes a filter and support for multiple groups
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name RemoveGroup -Value {
+        Param (
+            [Parameter(Mandatory)]
+            [string]$Name
+        )
+
+        $Groups = ForEach ($Group in $this.MemberOf)
+        {
+            If ($Group -match "cn=(?<GroupName>.*?)(?<!\\),")
+            {
+                $GroupName = $Matches.GroupName
+                If ($GroupName -match $Name)
+                {
+                    [PSCustomObject]@{
+                        Name = $GroupName
+                        DistinguishedName = $Group
+                    }
+                }
+            }
+        }
+        If (@($Groups).Count -gt 1)
+        {
+            $Remove = $Groups | Out-GridView -Title "Which groups do you want to remove from $($this.Name)?" -OutputMode Multiple
+        }
+        ElseIf (@($Groups).Count -eq 1) 
+        {
+            $Remove = $Groups
+        }
+        Else
+        {
+            Write-Verbose "No groups matched $Name"
+            Return
+        }
+
+        ForEach ($Group in $Remove)
+        {
+            $GroupObj = [ADSI]"LDAP://$($Group.DistinguishedName)"
+            $GroupObj.Remove("LDAP://$($this.DistinguishedName)")
+        }
+    }
+
+    #Add Group method
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name AddGroup -Value {
+        Param (
+            [Parameter(Mandatory)]
+            [string]$Name
+        )
+
+        Add-ADGroupMember -Identity $Name -Members $this.SamAccountName
+    }
+    <#
+    
+
+
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name GetLastLogon -Value {
         $DCs = Get-ADDomainController -Filter * | Select -ExpandProperty Name
         $Count = 1
         $DCData = ForEach ($DC in ($DCs))
@@ -190,33 +233,18 @@
         $this.LastLogon = $DCData | Sort "Last Logon" -Descending | Select -First 1 | Select -ExpandProperty "Last Logon"
         $this
     }
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name AddGroup -Value {
-        Param (
-            [Parameter(Mandatory)]
-            [string]$Name
-        )
 
-        Add-ADGroupMember -Identity $Name -Members $this.SamAccountName
-    }
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name RemoveGroup -Value {
-        Param (
-            [Parameter(Mandatory)]
-            [string]$Name
-        )
 
-        Remove-ADGroupMember -Identity $Name -Members $this.SamAccountName -Confirm:$false
-    }
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name ResetPassword -Value {
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name ResetPassword -Value {
         Set-ADAccountPassword -Identity $this.SamAccountName -Reset
     }
-    $AHUser | Add-Member -Force -MemberType ScriptMethod -Name FindLockout -Value {
+    $SAUser | Add-Member -Force -MemberType ScriptMethod -Name FindLockout -Value {
         Find-LockOut $this.SamAccountName
     }
-
-    $AHUser
-#>
+    #>
+    $SAUser
 }
 
 
 
-Get-SAUser -User mpugh | fl *
+Get-SAUser -User mpugh #| fl *
