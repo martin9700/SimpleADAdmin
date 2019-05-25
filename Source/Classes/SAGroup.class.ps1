79196aaea292e2b543ff71e9912f036da7343386
Martin Pugh <mpugh@athenahealth.com>
2019-05-25 15:51:28 -0400

    Converted Get-SAGroup to use a class

---
+++ Source/Classes/SAGroup.class.ps1
@@ -0,0 +1,166 @@
+﻿Class SAGroup
+{
+    [string]   $Name
+    [string]   $SamAccountName
+    [string]   $Description
+    [string]   $Info
+    [string]   $Email
+    [string]   $DistinguishedName
+    [guid]     $ObjectGUID
+    [string]   $ObjectSID
+    [string[]] $MemberOf
+    [string[]] $Members
+    [string]   $ManagedBy
+    [string]   $GroupCategory
+    [string]   $GroupScope
+    [datetime] $WhenCreated
+    
+    SAGroup ()
+    {
+        Write-Error "You must specify the SamAccountName of the group you want to look at" -ErrorAction Stop
+    }
+
+    SAGroup ( [string]$Identity )
+    {
+        $Searcher = [ADSISearcher]"(&(objectClass=group)(samAccountName=$Identity))"
+        $Found = @($Searcher.FindOne())
+
+        If ($null -eq $Found.properties.samaccountname)
+        {
+            Write-Error "Unable to find ""$Identity""" -ErrorAction Stop
+        }
+        $this.SamAccountName       = $Found.properties.samaccountname | Select-Object -First 1
+
+        $this.EnumerateFields()
+
+        $this | Add-Member -MemberType ScriptProperty -Name LastModified -Value {
+            $Searcher = [ADSISearcher]"(&(objectClass=group)(samAccountName=$($this.SamAccountName)))"
+            $Found = $Searcher.FindOne()
+
+            # Enumerate LastModified
+            Write-Output $Found.properties.whenchanged | Select-Object -First 1
+
+            $this.EnumerateFields()
+        }
+
+        $DefaultDisplayProperties = @(
+            "Name"
+            "SamAccountName"
+            "Description"
+            "Info"
+            "Email"
+            "DistinguishedName"
+            "ObjectGUID"
+            "ObjectSID"
+            "MemberOf"
+            "Members"
+            "ManagedBy"
+            "GroupCategory"
+            "GroupScope"
+            "WhenCreated"
+            "LastModified"
+        )
+        $this | Add-Member -Force -MemberType MemberSet PSStandardMembers ([System.Management.Automation.PSMemberInfo[]]@(New-Object System.Management.Automation.PSPropertySet("DefaultDisplayPropertySet",[String[]]$DefaultDisplayProperties)))
+
+    }
+
+    hidden [void] EnumerateFields()
+    {
+        $Searcher = [ADSISearcher]"(&(objectClass=group)(samAccountName=$($this.SamAccountName)))"
+        $Found = @($Searcher.FindOne())
+
+        #Thanks Richard Siddaway for breaking this out
+        $GroupTypes = @{
+            2           = [PSCustomObject]@{Category="Distribution";Scope="Global"}
+            4           = [PSCustomObject]@{Category="Distribution";Scope="DomainLocal"}
+            8           = [PSCustomObject]@{Category="Distribution";Scope="Universal"}
+            -2147483646 = [PSCustomObject]@{Category="Security";Scope="Global"}
+            -2147483644 = [PSCustomObject]@{Category="Security";Scope="DomainLocal"}
+            -2147483643 = [PSCustomObject]@{Category="Security";Scope="BuiltinLocal"}
+            -2147483640 = [PSCustomObject]@{Category="Security";Scope="Universal"}
+        }
+        
+        $this.Name                 = $Found.properties.name | Select-Object -First 1
+        $this.Description          = $Found.properties.description | Select-Object -First 1
+        $this.Info                 = $Found.properties.info | Select-Object -First 1
+        $this.Email                = $Found.properties.mail | Select-Object -First 1
+        $this.DistinguishedName    = $Found.properties.distinguishedname | Select-Object -First 1
+        $this.ObjectGUID           = New-Object GUID(,($Found.properties.objectguid | Select-Object -First 1))
+        $this.ObjectSID            = (New-Object System.Security.Principal.SecurityIdentifier(($Found.properties.objectsid | Select-Object -First 1),0)).Value
+        $this.MemberOf             = $Found.properties.memberof
+        $this.Members              = $Found.properties.member
+        $this.ManagedBy            = $Found.properties.managedby | Select-Object -First 1
+        $this.GroupCategory        = $GroupTypes[($Found.properties.grouptype | Select-Object -First 1)].Category
+        $this.GroupScope           = $GroupTypes[($Found.properties.grouptype | Select-Object -First 1)].Scope
+        $this.WhenCreated          = $Found.properties.whencreated | Select-Object -First 1
+    }
+
+    hidden [PSCustomObject[]] GetMemberObject([string]$dn, [boolean]$Recurse)
+    {
+        $Results = New-Object -TypeName System.Collections.ArrayList
+        $Searcher = [ADSISearcher]"((distinguishedName=$dn))"
+        $Found = @($Searcher.FindOne())
+        If ($Found.Count -eq 0)
+        {
+            Write-Warning "Unable to find ""$dn"""
+        }
+        Else
+        {
+            If (($Found.properties.objectclass | Select-Object -Last 1) -eq "group" -and $Recurse)
+            {
+                $subResults = $this.GetMembersFromGroup($dn)
+                ForEach ($Result in $subResults)
+                {
+                    $null = $Results.Add($Result)
+                }
+            }
+            Else
+            {
+                $null = $Results.Add([PSCustomObject]@{
+                    Member         = $Found.properties.name | Select-Object -First 1
+                    SamAccountName = $Found.properties.samaccountname | Select-Object -First 1
+                    ObjectClass    = $Found.properties.objectclass | Select-Object -Last 1
+                })
+            }
+        }
+        Return $Results
+    }
+
+    hidden [PSCustomObject[]] GetMembersFromGroup ([string]$dn)
+    {
+        $Results = $null
+        $Searcher = [ADSISearcher]"((distinguishedName=$dn))"
+        $Found = @($Searcher.FindOne())
+        If ($Found.Count -eq 0)
+        {
+            Write-Warning "Unable to find ""$dn"""
+        }
+        Else
+        {
+            $Results = ForEach ($Member in ($Found.properties.member))
+            {
+                $this.GetMemberObject($Member, $true)
+            }
+        }
+
+        Return $Results
+    }
+
+    [PSCustomObject[]] GetMembers()
+    {
+        $Results = ForEach ($Member in $this.Members)
+        {
+            $this.GetMemberObject($Member, $false)
+        }
+        Return $Results
+    }
+
+    [PSCustomObject[]] GetMembersRecursive()
+    {
+        $Results = ForEach ($Member in $this.Members)
+        {
+            $this.GetMemberObject($Member, $true)
+        }
+        Return $Results
+    }
+}
--- Source/Public/Get-SAGroup.ps1
+++ Source/Public/Get-SAGroup.ps1
@@ -3,12 +3,16 @@
     #>
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true,
+            Position = 0)]
         [Alias("Group","SamAccountName","Name")]
-        [string]$Identity,
-        [string[]]$DefaultDisplayProperties = @("Name","SamAccountName","Description","DistinguishedName","GroupCategory","GroupScope")
+        [string]$Identity
     )
 
+    # Clear old variable
+    Remove-Variable -Name SAGroup -Scope Global
+
+    # Find the group
     $Searcher = [ADSISearcher]"(&(objectClass=group)(samAccountName=$Identity))"
     $Found = @($Searcher.FindAll())
     If ($Found.Count -ne 1)
@@ -21,11 +25,12 @@
         }
         If ($Found.Count -gt 1)
         {
-            #Found more than one, need to select which one you want
+            # Found more than one, need to select which one you want
             $Selected = $Found | 
                 Select @{Name="SamAccountName";Expression={ $_.properties.samaccountname }},
                     @{Name="DisplayName";Expression={ $_.properties.displayname }},
                     @{Name="Description";Expression={ $_.properties.description }} | 
+                Sort-Object -Property SamAccountName |
                 Out-GridView -Title "Select the correct group" -PassThru
             If (@($Selected).Count -eq 0)
             {
@@ -41,44 +46,7 @@
         }
     }
 
-    #Thanks Richard Siddaway for breaking this out
-    $GroupTypes = @{
-        2           = [PSCustomObject]@{Category="Distribution";Scope="Global"}
-        4           = [PSCustomObject]@{Category="Distribution";Scope="DomainLocal"}
-        8           = [PSCustomObject]@{Category="Distribution";Scope="Universal"}
-        -2147483646 = [PSCustomObject]@{Category="Security";Scope="Global"}
-        -2147483644 = [PSCustomObject]@{Category="Security";Scope="DomainLocal"}
-        -2147483643 = [PSCustomObject]@{Category="Security";Scope="BuiltinLocal"}
-        -2147483640 = [PSCustomObject]@{Category="Security";Scope="Universal"}
-    }
-    
-    $Global:SAGroup = [PSCustomObject]@{
-        Name                 = $Found.properties.name | Select -First 1
-        SamAccountName       = $Found.properties.samaccountname | Select -First 1        
-        Description          = $Found.properties.description | Select -First 1
-        Info                 = $Found.properties.info | Select -First 1
-        Email                = $Found.properties.mail | Select -First 1
-        DistinguishedName    = $Found.properties.distinguishedname | Select -First 1
-        ObjectGUID           = New-Object GUID(,($Found.properties.objectguid | Select -First 1))
-        ObjectSID            = (New-Object System.Security.Principal.SecurityIdentifier(($Found.properties.objectsid | Select -First 1),0)).Value
-        MemberOf             = $Found.properties.memberof
-        Members              = $Found.properties.member
-        ManagedBy            = $Found.properties.managedby | Select -First 1
-        GroupCategory        = $GroupTypes[($Found.properties.grouptype | Select -First 1)].Category
-        GroupScope           = $GroupTypes[($Found.properties.grouptype | Select -First 1)].Scope
-        WhenCreated          = $Found.properties.whencreated | Select -First 1
-    }
-
-    #Display default properties
-    $SAGroup | Add-Member -Force -MemberType MemberSet PSStandardMembers ([System.Management.Automation.PSMemberInfo[]]@(New-Object System.Management.Automation.PSPropertySet("DefaultDisplayPropertySet",[String[]]$DefaultDisplayProperties)))
-
-    #Last modified, and dynamic fields
-    $SAGroup | Add-Member -MemberType ScriptProperty -Force -Name LastModified -Value {
-        $Searcher = [ADSISearcher]"(&(objectClass=group)(samAccountName=$($this.SamAccountName)))"
-        $Found = $Searcher.FindOne()
-
-        $Found.properties.whenchanged | Select -First 1
-        $this.MemberOf  = $Found.properties.memberof
-        $this.Members   = $Found.properties.member
-    }
+    # Set global variable and return object
+    $Global:SAGroup = [SAGroup]::New($Found.properties.samaccountname)
+    Return $Global:SAGroup
 }
